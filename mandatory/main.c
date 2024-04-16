47c90f2ed0f76d2633906ceff77e3ca6909f6974
Mauricio Rodriguez <maurodri@student.42sp.org.br>
2024-04-16 20:55:08 -0300

    2 children

--- mandatory/main.c
+++ mandatory/main.c
@@ -6,40 +6,80 @@
 /*   By: maurodri <maurodri@student.42sp...>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 20:34:19 by maurodri          #+#    #+#             */
-/*   Updated: 2024/04/08 13:20:07 by maurodri         ###   ########.fr       */
+/*   Updated: 2024/04/16 20:54:42 by maurodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_stdio.h"
+#include "ft_memlib.h"
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-int	main(void)
+#define STDIN  0
+#define STDOUT 1
+#define EXIT_OK 0
+#define EXIT_PIPE_FAIL 1
+#define EXIT_FORK_FAIL 2
+
+void child_0()
 {
-	//pid_t pid;
+	char	str[100];
 	
-	/* pid = fork(); */
-	/* if (pid == 0) */
-	/* 	ft_printf("hello child pid=%d\n", pid); */
-	/* else */
-	/* 	ft_printf("hello parent child_pid=%d\n", pid); */
-	/* dup2() */
+	ft_bzero(str, 100);
+	read(STDIN, str, 16);
+	ft_printf("child0 afer dup2\nmessage: %s\n", str);
+}
+
+void child_1()
+{
+	ft_printf("child1 afer dup2");
+}
+
+int is_child(pid_t pid)
+{
+	return (pid == 0);
+}
+
+int	main(int argc, char* argv[])
+{
+	(void) argc;
+	(void) argv;
+
+	pid_t	pid[2];
+	int		fds[2]; //fds[0] read, fds[1] write
+
+	//char	*fileIn = argv[1];
+	//char	*fileOut = argv[2];
+	//char	*command0 = argv[3];
+	//char	*command1 = argv[4];
 	
-	int fd = open("test.txt", O_CREAT | O_RDWR, 0666);
-	ft_printf("fd=%d\n", fd);
-	dup2(fd, 1); // closes current 1 and make 1 point to fd,
-	             // both fd and 1 will point to same file descriptor  
-	ft_printf("fd=%d\n", fd);
-	write(fd, "heyfd\n", 6);
-	write(1, "hey1\n", 5);
-
-	/////////
-	int fds[2]; //fds[0] read, fds[1] write
-	pipe(fds);
-
-	write(fds[1], "hola\n", 5);
-	char out[10];
-	read(fds[0], out, 5);
-	ft_printf("out=%s\n", out);
+	if (pipe(fds) < 0)
+		return (EXIT_PIPE_FAIL);
+	pid[0] = fork();
+	if (pid[0] < 0)
+		return (EXIT_FORK_FAIL);
+	if (is_child(pid[0]))
+	{
+		dup2(fds[0], STDIN);
+		close(fds[1]);
+		child_0();
+		return (0);
+	}
+	pid[1] = fork();
+	if (pid[1] < 0)
+		return (EXIT_FORK_FAIL);
+	if(is_child(pid[1]))
+	{
+		dup2(fds[1], STDOUT);
+		close(fds[0]);
+		child_1();
+		return (0);
+	}
+	close(fds[0]);
+	close(fds[1]);
+	waitpid(pid[0], NULL, 0);
+	waitpid(pid[1], NULL, 0);
 	return (0);
 }
